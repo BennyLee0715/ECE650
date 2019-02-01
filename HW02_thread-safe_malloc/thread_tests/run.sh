7a30eeaf9225ce16d78f17a0dc525014dfdaf128
Yifan Men <menyifan0001@gmail.com>
2019-02-01 10:28:42 -0500

    Passed 200 tests

--- HW02_thread-safe_malloc/thread_tests/run.sh
+++ HW02_thread-safe_malloc/thread_tests/run.sh
@@ -1,17 +1,17 @@
 
-echo "===================Making===================="
+echo "===================Making================================================="
 make clean
 make
-for i in {1..50}
+for i in {1..200}
          do
- echo "====================Testing thread_test===================="
+ echo "====================$i Testing thread_test==============================="
  ./thread_test
- echo "====================Testing thread_test_malloc_free===================="
+ echo "====================Testing thread_test_malloc_free======================"
  ./thread_test_malloc_free
- echo "====================$i Testing thread_test_malloc_free_change_thread===================="
+ echo "====================$i Testing thread_test_malloc_free_change_thread====="
  ./thread_test_malloc_free_change_thread #> tmp.txt
- echo "====================$i thread_test_measurement===================="
+ echo "====================$i thread_test_measurement==========================="
  ./thread_test_measurement
 done
-echo "===================Cleaning===================="
+echo "===================Cleaning==============================================="
 make clean
